732518e164618dee8a93dde3175ca74d2d24b761
Min-jae Kim <mink4661@snu.ac.kr>
2023-01-10 19:37:36 +0900

    api test

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Body from '../components/layout/body';
 import Container from '../components/layout/container';
@@ -6,7 +7,16 @@ import Header from '../components/layout/header';
 import { open } from '../store/sideBarSlice';
 
 export default function Main() {
+  const [data, setdata] = useState<any>([]);
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_DOMAIN + '/api/')
+      .then((response) => response.json())
+      .then((json) => setdata(json))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <Container>
@@ -14,7 +24,10 @@ export default function Main() {
       <Body>
         제 페이지에 오신 것을 환영합니다! <br />
         다양한 기능이 추가될 예정입니다! Coming Soon! <br />
-        기대해 주세요!
+        기대해 주세요!{' '}
+        {data.map((e: any) => (
+          <div>{e.title}</div>
+        ))}
       </Body>
     </Container>
   );
