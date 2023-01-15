a4fc2a9123c977671a23063c78312804c353dd32
Min-jae Kim <mink4661@snu.ac.kr>
2023-01-15 23:59:22 +0900

    기본 폰트 적용

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,11 @@ import { store } from './store/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
