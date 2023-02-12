6f0f14cc48b1761c4584c804e6c614f1b2c9cb22
Min-jae Kim <mink4661@snu.ac.kr>
2023-02-12 23:06:05 +0900

    three 설치

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,9 +6,6 @@ import CommonStructure from '../components/structure/commonStructure';
 export default function Main() {
   return (
     <CommonStructure title="홈">
-      <div style={{ height: '10px' }}></div>
-      제 페이지에 오신 것을 환영합니다. <br />
-      <br />
       현재 만들어진 기능(2023.01.10.):
       <br />- 방명록 기능 (상단 MENU 클릭 {'>'} 방명록 클릭)
     </CommonStructure>
